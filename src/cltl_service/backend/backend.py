2015f1f24e5083a488be227ff407b9f75ab03dea
Thomas Baier <tbaier@math.bme.hu>
2022-04-21 09:37:12 +0200

    Add scenario id when creating signals

--- src/cltl_service/backend/backend.py
+++ src/cltl_service/backend/backend.py
@@ -8,7 +8,7 @@ from cltl.combot.infra.time_util import timestamp_now
 from cltl.combot.infra.topic_worker import TopicWorker
 from cltl.combot.infra.util import ThreadsafeBoolean
 from emissor.representation.scenario import AudioSignal, ImageSignal
-from threading import Thread
+from threading import Thread, Lock
 
 from cltl.backend.api.backend import Backend
 from cltl.backend.api.camera import Image
@@ -23,6 +23,9 @@ class BackendService:
     @classmethod
     def from_config(cls, backend: Backend, audio_storage: AudioStorage, image_storage: ImageStorage,
                     event_bus: EventBus, resource_manager: ResourceManager, config_manager: ConfigurationManager):
+        config = config_manager.get_config("cltl.backend")
+        scenario_topic = config.get("scenario_topic")
+
         config = config_manager.get_config("cltl.backend.mic")
         mic_topic = config.get('topic')
 
@@ -33,26 +36,31 @@ class BackendService:
         config = config_manager.get_config("cltl.backend.tts")
         tts_topic = config.get('topic')
 
-        return cls(mic_topic, image_topic, tts_topic, image_rate, backend, audio_storage, image_storage,
-                   event_bus, resource_manager)
+        return cls(mic_topic, image_topic, tts_topic, scenario_topic, image_rate,
+                   backend, audio_storage, image_storage, event_bus, resource_manager)
 
-    def __init__(self, mic_topic: str, image_topic: str, tts_topic: str, image_rate: float,
+    def __init__(self, mic_topic: str, image_topic: str, tts_topic: str, scenario_topic: str,
+                 image_rate: float,
                  backend: Backend, audio_storage: AudioStorage, image_storage: ImageStorage,
                  event_bus: EventBus, resource_manager: ResourceManager):
         self._mic_topic = mic_topic
         self._image_topic = image_topic
         self._tts_topic = tts_topic
+        self._scenario_topic = scenario_topic
+
         self._image_rate = image_rate
 
         self._backend = backend
         self._running = ThreadsafeBoolean()
 
-        # TODO
+        self._scenario_lock = Lock()
         self._scenario_id = None
+        self._require_scenario = bool(self._scenario_topic)
 
         self._mic_thread = None
         self._image_thread = None
-        self._topic_worker = None
+        self._tts_worker = None
+        self._scenario_worker = None
 
         self._audio_storage = audio_storage
         self._image_storage = image_storage
@@ -64,42 +72,56 @@ class BackendService:
         return None
 
     def start(self):
-        # TODO
-        self._scenario_id = str(uuid.uuid4())
-
         self._running.value = True
 
         self._backend.start()
+        self._start_scenario()
         self._start_mic()
         self._start_image()
         self._start_tts()
 
     def stop(self):
-        # TODO
-        self._scenario_id = None
+        self._scenario_id = None if self._require_scenario else str(uuid.uuid4())
 
         self._running.value = False
 
         self._stop_tts()
         self._stop_image()
         self._stop_mic()
+        self._stop_scenario()
         self._backend.stop()
 
+    def _start_scenario(self):
+        self._scenario_worker = TopicWorker([self._scenario_topic],
+                                         event_bus=self._event_bus,
+                                         resource_manager=self._resource_manager,
+                                         processor=self._process_scenario,
+                                         name=self.__class__.__name__ + "_scenario")
+        self._scenario_worker.start().wait()
+
+    def _stop_scenario(self):
+        if not self._scenario_worker:
+            pass
+
+        self._scenario_worker.stop()
+        self._scenario_worker.await_stop()
+        self._scenario_worker = None
+
     def _start_tts(self):
-        self._topic_worker = TopicWorker([self._tts_topic],
+        self._tts_worker = TopicWorker([self._tts_topic],
                                          event_bus=self._event_bus,
                                          resource_manager=self._resource_manager,
                                          processor=self._process_tts,
-                                         name=self.__class__.__name__)
-        self._topic_worker.start().wait()
+                                         name=self.__class__.__name__ + "_tts")
+        self._tts_worker.start().wait()
 
     def _stop_tts(self):
-        if not self._topic_worker:
+        if not self._tts_worker:
             pass
 
-        self._topic_worker.stop()
-        self._topic_worker.await_stop()
-        self._topic_worker = None
+        self._tts_worker.stop()
+        self._tts_worker.await_stop()
+        self._tts_worker = None
 
     def _start_image(self):
         if self._image_thread:
@@ -110,10 +132,15 @@ class BackendService:
 
         def run():
             while self._running:
+                if not self.scenario_id:
+                    logger.info("No active scenario, waiting to start recording images")
+                    time.sleep(1)
+                    continue
                 try:
                     self._record_images()
                 except IOError as e:
-                    logger.warning("Failed to capture to image: %s", e)
+                    # ConnectionErrors may occur during start-up
+                    logging.warning("Failed to capture to image: %s", e)
                     time.sleep(1)
                 except Exception as e:
                     logger.exception("Failed to capture to image: %s", e)
@@ -135,6 +162,10 @@ class BackendService:
 
         def run():
             while self._running:
+                if not self.scenario_id:
+                    logger.info("No active scenario, waiting to start listening")
+                    time.sleep(1)
+                    continue
                 try:
                     audio_id = str(uuid.uuid4())
                     with self._backend.microphone.listen() as (audio, params):
@@ -143,8 +174,8 @@ class BackendService:
                                                   params.sampling_rate)
                         logger.info("Stored audio %s", audio_id)
                 except IOError as e:
-                    # Log ConnectionErrors as warnings, those may occur especially during start-up
-                    logger.warning("Failed to listen to mic: %s", e)
+                    # ConnectionErrors may occur during start-up
+                    logging.warning("Failed to listen to mic: %s", e)
                     time.sleep(1)
                 except Exception as e:
                     logger.exception("Failed to listen to mic: %s", e)
@@ -159,6 +190,16 @@ class BackendService:
 
         self._mic_thread.join()
         self._mic_thread = None
+
+    @property
+    def scenario_id(self):
+        with self._scenario_lock:
+            return self._scenario_id
+
+    @scenario_id.setter
+    def scenario_id(self, scenario_id: str):
+        with self._scenario_lock:
+            self._scenario_id = scenario_id
 
     def _record_images(self):
         with self._backend.camera as camera:
@@ -200,17 +241,21 @@ class BackendService:
             event = Event.for_payload(stopped)
             self._event_bus.publish(self._mic_topic, event)
 
+    def _process_scenario(self, event: Event):
+        logger.debug("Process Scenario event %s", event.payload)
+        self.scenario_id = event.payload.scenario.id
+
     def _process_tts(self, event: Event):
-        logger.info("Process TTS event %s", event.payload)
+        logger.debug("Process TTS event %s", event.payload)
         self._backend.text_to_speech.say(event.payload.signal.text)
 
     def _create_audio_signal(self, audio_id: str, parameters: AudioParameters,
                             start: int = None, stop: int = None, length: int = None):
-        return AudioSignal.for_scenario(self._scenario_id, start, stop,
+        return AudioSignal.for_scenario(self.scenario_id, start, stop,
                                         f"cltl-storage:audio/{audio_id}",
                                         length, parameters.channels, signal_id=audio_id)
 
     def _create_image_signal(self, image_id, image):
-        return ImageSignal.for_scenario(self._scenario_id, timestamp_now(), timestamp_now(),
+        return ImageSignal.for_scenario(self.scenario_id, timestamp_now(), timestamp_now(),
                                         f"cltl-storage:image/{image_id}",
                                         image.bounds.to_diagonal(), signal_id=image_id)
